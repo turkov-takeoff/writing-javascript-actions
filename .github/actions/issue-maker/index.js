d9b86f799e78d65dc3daf0505118ad027af5feb7
oturkov <oleksandr.turkov@takeoff.com>
2021-09-07 17:42:20 +0300

    added logging

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -8,6 +8,7 @@ async function run() {
     const token = core.getInput("repo-token");
 
     const octokit = new github.getOctokit(token);
+    console.log(octokit);
 
     const newIssue = await octokit.issues.create({
       repo: github.context.repo.repo,
